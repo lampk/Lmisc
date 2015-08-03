838a7cd5a25b3cf318cfc1a98db8d03ead2cfe85
lampk <lampk.biostat@gmail.com>
2015-08-03 14:19:52 +0700

    add package c060 to description

--- misc.R
+++ misc.R
@@ -1,6 +1,6 @@
 ## packages need to be import
 pkg <- c("RCurl", "car", "lubridate", "mice", "survival", "glmnet", "penalized",
-         "ncvreg", "mgcv", "rpart", "randomForest", "gbm", "glmulti", "caret", "ROCR")
+         "ncvreg", "mgcv", "rpart", "randomForest", "gbm", "glmulti", "caret", "ROCR", "c060")
 sapply(pkg, function(x) devtools::use_package(x))
 
 ## build documentation
