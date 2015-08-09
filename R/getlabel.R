461cbf87e7370909bd5b05c0b95e21ca8d6b80e9
lampk <phungkhanhlam@gmail.com>
2015-08-09 07:39:18 +0700

    move getlabel.* and mySummary.* into one file

--- R/getlabel.R
+++
@@ -1,8 +0,0 @@
-#' @export
-getlabel <- function(x, meta = NULL, units = TRUE, fit = FALSE){
-  if (is.null(dim(x))) {
-    getlabel.one(x, meta = meta, units = units, fit = fit)
-  } else {
-    getlabel.data(x, meta = meta, units = units, fit = fit)
-  }
-}
