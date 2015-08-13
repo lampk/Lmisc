76900a72d4dcae606c9e208fa029e05f0a822738
lampk <lampk.biostat@gmail.com>
2015-08-13 11:16:59 +0700

    add and export cite.soft

---
+++ R/cite.soft.R
@@ -0,0 +1,29 @@
+#' To create BibTex file for references of software
+#'
+#' @param soft list of citation or package want to reference
+#' @param key key for inline citation
+#' @param output output file
+#'
+#' @return Bibtex file
+#' @export
+cite.soft <- function(soft, key, output = "software.bib") {
+
+  capture.output(invisible(mapply(function(key_i, soft_i) {
+    ## get citation
+    if ("citation" %in% class(soft_i)) {
+      tmp <- add.key(key_i, soft_i)
+    } else {
+      tmp <- add.key(key_i, citation(soft_i)[1])
+    }
+    ## output
+    print(toBibtex(tmp))
+    },
+         key_i = key, soft_i = soft)), file = output)
+}
+
+add.key <- function(key, obj){
+  # to add key into obj
+  obj$key <- key
+  return(obj)
+}
+
