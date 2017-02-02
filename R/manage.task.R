2ae70d064b675c1c47190ce0e2a14074421564d0
lampk <phungkhanhlam.@gmail.com>
2017-02-03 00:41:18 +0700

    add addin insert date

--- R/manage.task.R
+++ R/manage.task.R
@@ -151,3 +151,31 @@ manage_task_addin <- function() {
   ## run
   runGadget(ui, server)
 }
+
+#' Addin to insert date
+#' @export
+insert_date_addin <- function() {
+  require(shiny)
+  require(miniUI)
+
+  ## ui
+  ui <- miniPage(
+    gadgetTitleBar("Insert Date",
+                   right = miniTitleBarButton("done", "Done", primary = TRUE)),
+    miniContentPanel(
+      dateInput("date", "Date:", value = Sys.Date())
+    )
+  )
+
+  ## server
+  server <- function(input, output, session){
+    ## stop app when click "Done"
+    observeEvent(input$done, {
+      rstudioapi::insertText(as.character(input$date))
+      stopApp()
+    })
+  }
+
+  ## run
+  runGadget(ui, server)
+}
